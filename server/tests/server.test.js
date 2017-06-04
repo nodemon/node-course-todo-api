96692036659ddb7456dff13d69c06afeb479d7c6
Nikhil Saxena <nikhil.saxena@ericsson.com>
2017-06-04 00:09:23 -0400

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -111,5 +111,45 @@ describe ('GET /todos/:id', () => {
       .end(done);
   });
 
+})
+
+describe ('DELETE /todos/:id', () => {
+  var id = testTodos[1]._id.toHexString();
+
+  it ('should remove a todo', (done) => {
+    request (app)
+      .delete(`/todos/${id}`)
+      .expect(200)
+      .expect ( (res) => {
+        expect(res.body.todo._id).toBe(id);
+      })
+      .end( (err, res) => {
+        if (err) {
+          return done (err);
+        }
+
+        Todo.findById(id).then( (todo)=> {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done (e))
+      })
+
+  });
+
+  it ('should return 404 if todo is not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request (app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done)
+  });
+
+  it ('should return 400 if id is invalid', (done) => {
+    request (app)
+      .delete(`/todos/123asdf`)
+      .expect(400)
+      .end(done)
+
+  });
 
 })
