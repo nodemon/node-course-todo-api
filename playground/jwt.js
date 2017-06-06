d90f8080f4b0db910ebe4a62fd5533bc1cf9fc34
Nikhil Saxena <nikhil.saxena@ericsson.com>
2017-06-06 11:41:59 -0400

    Hash user password before save

--- playground/jwt.js
+++ playground/jwt.js
@@ -1,13 +1,41 @@
+// jwt is used to generate hash and verify hashed data
+// use www.jwt.io to view tokens
 const jwt = require('jsonwebtoken');
 
 var data = {
   id: 10
 };
 
-// use www.jwt.io to view tokens
-
 var token = jwt.sign(data, '123abc');
 console.log(token);
 
 var decoded = jwt.verify(token, '123abc');
 console.log('decoded ', decoded);
+
+
+
+// brypt JS; has salting built in
+ // - salt is actually stored in the string before the hash value.
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+
+bcrypt.genSalt(10, (err, salt) => { // generates a salt randomly 10 rounds
+  bcrypt.hash(password, salt, (err, hash) => { // use the salt to generate hash
+    console.log(hash);
+  } )
+});
+
+// This string has both salt and hash
+var hashedPassword = '$2a$10$jY1WL5u4rvDLz9mkaaMzlOeX8mhVUrr82v6PLZmVCbzN4PC93DDm2';
+
+/*
+'$2a$10$ - algorithm and the number of rounds to use
+ jY1WL5u4rvDLz9mkaaMzlO 22 character salt stored inline, so it doesnt need to be stored as a separate database field
+ eX8mhVUrr82v6PLZmVCbzN4PC93DDm2 31 character hash of the password (using the above salt).
+*/
+
+bcrypt.compare('123abc!', hashedPassword, (err, res) => {
+  console.log(res);
+})
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email : {
@@ -81,6 +82,23 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access' : 'auth'
   })
 }
+
+// Mongoose allows middleware methods to be added based on certain events
+// Define a middleware to run before UserSchema document is saved: 'pre' 'save'
+UserSchema.pre('save', function (next) {
+  var user = this;
+  if (user.isModified('password')) {
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next();
+  }
+})
 
 var User = mongoose.model('User', UserSchema);
 
